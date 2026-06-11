f2af21bcdf58bf5a8ddcfefe95b313cca78d0296
Laradomingos21 <lara.v.domingos@aluno.senai.br>
2026-06-11 16:00:58 -0300

    tcc

--- sistema/sistema.js
+++
@@ -1,5 +0,0 @@
-nomeSistema = document.querySelector("#nome-sistema");
-nomeSistema.addEventListener("click", function() {
-    window.location.href = "index_sistema.php";
-});
-console.log("Sistema carregado com sucesso!");
